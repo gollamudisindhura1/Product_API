9fde3193eb54287049afe69673ec44b2618730c3
Sindhura Gollamudi <gollamudisindhura1@gmail.com>
2026-01-17 15:51:05 -0500

    Added Products

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,39 @@ const productSchema = new Schema({
         type: String,
         required: true,
         message: "Name is required."
-    }
+    },
+    description: {
+        type: String,
+        required: true,
+        message : ""
+    },
 
-})
+  price: {
+    type: Number,
+    required: true,
+    min: [1, "Price must be greater than 0"]
+  },
+
+  category: {
+    type: String,
+    required: true,
+    message: "Category is required"
+  },
+
+  inStock: {
+    type: Boolean,
+    default: true
+  },
+
+  tags: {
+    type: [String]
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Product = mongoose.model("Product", productSchema)
+module.exports = Product;
